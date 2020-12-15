61b193557b059739ec4fd94da7564a3000afedf0
ralles <ralles.liu@gmail.com>
2020-12-15 17:46:03 -0500

    phase 3 finished

---
+++ skeleton/phase_3_iteration.js
@@ -0,0 +1,36 @@
+Array.prototype.bubbleSort = function() {
+    let flag = true;
+
+    while (flag) {
+        flag = false;
+
+        for (let i=0; i < this.length - 1; i++) {   
+            if (this[i] > this[i+1]) {
+                flag = true;
+                let tempFirst = this[i];
+                let tempSecond = this[i+1];
+                this[i] = tempSecond;
+                this[i+1] = tempFirst;
+                // console.log(this);
+            }
+        }
+    }
+    return this;
+};
+
+
+// console.log([11,3,5,6,2,34,6].bubbleSort());
+
+
+String.prototype.substrings = function() {
+    let new_arr = [];
+    for (let i = 0; i < this.length; i++) {  
+        for (let j = i; j < this.length; j++) {
+            new_arr.push(this.slice(i, j+1));
+        }   
+    }
+    return new_arr;
+};
+
+console.log("hello".substrings());
+
